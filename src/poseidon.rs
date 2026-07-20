6b64673a0b8e5269fe2fbbb55070eab9253cd8f0
71104 <a71104@gmail.com>
2026-07-20 22:44:26 +0200

    Add doc snippet.

--- src/poseidon.rs
+++ src/poseidon.rs
@@ -73,6 +73,7 @@ fn internal_linear<
     linear::<F, T>(Cfg::get_internal_matrix(), state)
 }
 
+/// Runs the Poseidon2 permutation.
 pub fn permutation<
     Cfg: Config<F, T, R, C>,
     F: PrimeField,
